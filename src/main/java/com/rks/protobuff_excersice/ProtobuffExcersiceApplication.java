7ea3ffd59fa830fa421da9b3cd5e4e9eff9b9f48
RakeshSwainDeveloper <rakesh.swain@netprophetsglobal.com>
2025-05-15 10:24:11 +0530

    Adding project to repository

---
+++ src/main/java/com/rks/protobuff_excersice/ProtobuffExcersiceApplication.java
@@ -0,0 +1,13 @@
+package com.rks.protobuff_excersice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobuffExcersiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtobuffExcersiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rks/protobuff_excersice/ProtobuffExcersiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rks.protobuff_excersice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobuffExcersiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
